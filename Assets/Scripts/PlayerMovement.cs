8d689ef78d0e23667d7e5e29a283f7de96fe9c91
emresisman <emresisman53@gmail.com>
2018-12-10 15:37:39 +0300

    Movement Test

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+    float _horizontalSpeed, _verticalSpeed;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,9 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        _horizontalSpeed = Mathf.Clamp(Input.GetAxis("Horizontal"), -0.2f, 0.2f);
+        _verticalSpeed = Mathf.Clamp(Input.GetAxis("Vertical"), -0.2f, 0.2f);
+        //Debug.Log(-_horizontalSpeed + " " + _verticalSpeed);
+        this.transform.Translate(_horizontalSpeed, _verticalSpeed, 0f);
 	}
 }
