8241294d1231b5703dbf42497bde05301578b6d6
emresisman <emresisman53@gmail.com>
2018-12-10 16:52:52 +0300

    Basic physics
    Movement, MouseLook, Background

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+
+	void Update () {
+
+        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(this.transform.position);
+        Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle - 90f));
+    }
+
+    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+    {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,16 +6,10 @@ public class PlayerMovement : MonoBehaviour {
 
     float _horizontalSpeed, _verticalSpeed;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
 	// Update is called once per frame
 	void Update () {
-        _horizontalSpeed = Mathf.Clamp(Input.GetAxis("Horizontal"), -0.2f, 0.2f);
-        _verticalSpeed = Mathf.Clamp(Input.GetAxis("Vertical"), -0.2f, 0.2f);
-        //Debug.Log(-_horizontalSpeed + " " + _verticalSpeed);
-        this.transform.Translate(_horizontalSpeed, _verticalSpeed, 0f);
+        _horizontalSpeed = Mathf.Clamp(Input.GetAxis("Horizontal"), -0.05f, 0.05f);
+        _verticalSpeed = Mathf.Clamp(Input.GetAxis("Vertical"), -0.05f, 0.05f);
+        this.transform.Translate(-_horizontalSpeed, -_verticalSpeed, 0f);
 	}
 }
