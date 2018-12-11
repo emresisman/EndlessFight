c8d3073553894eece3b1acd8ea9358717e4c2125
emresisman <emresisman53@gmail.com>
2018-12-11 16:26:43 +0300

    Joystick controller
    Added two joystick button. One for movement, other one for direction. Added player camera.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     float _horizontalSpeed, _verticalSpeed;
+    public FixedJoystick _joystick;
 
 	// Update is called once per frame
 	void Update () {
-        _horizontalSpeed = Mathf.Clamp(Input.GetAxis("Horizontal"), -0.05f, 0.05f);
-        _verticalSpeed = Mathf.Clamp(Input.GetAxis("Vertical"), -0.05f, 0.05f);
-        this.transform.Translate(-_horizontalSpeed, -_verticalSpeed, 0f);
-	}
+        //_horizontalSpeed = Mathf.Clamp(Input.GetAxis("Horizontal"), -0.05f, 0.05f);
+        //_horizontalSpeed = Mathf.Clamp(_joystick.Horizontal, -0.05f, 0.05f);
+        //_verticalSpeed = Mathf.Clamp(Input.GetAxis("Vertical"), -0.05f, 0.05f);
+        //_verticalSpeed = Mathf.Clamp(_joystick.Vertical, -0.05f, 0.05f);
+        //this.transform.Translate(-_horizontalSpeed, -_verticalSpeed, 0f);
+        //this.transform.position += new Vector3(_horizontalSpeed, _verticalSpeed, 0f);
+
+
+        Vector3 moveVector = (Vector3.right * _joystick.Horizontal + Vector3.up * _joystick.Vertical);
+
+        if (moveVector != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(Vector3.forward, moveVector);
+            transform.Translate(moveVector * 8f * Time.deltaTime, Space.World);
+        }
+    }
 }
