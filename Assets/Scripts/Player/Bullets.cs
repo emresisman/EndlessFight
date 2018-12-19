3f3609ca5667176fcf939a646a02339e10ffc1e3
emresisman <emresisman53@gmail.com>
2018-12-19 16:54:29 +0300

    Bullet
    Started bullet properties.

---
+++ Assets/Scripts/Player/Bullets.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullets : MonoBehaviour {
+
+    private SpriteRenderer _sprites;
+
+	void Start () {
+        _sprites = this.GetComponent<SpriteRenderer>();
+        _sprites.sprite=Resources.Load("Sprites/Player/Bullets/Bullet1", typeof(Sprite)) as Sprite;
+    }
+	
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            Destroy(this.gameObject);
+        }
+        else if (collision.tag == "DangerZone")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
