ea9fb3281a0bb278ff0974794fe6e88f612e7546
emresisman <emresisman53@gmail.com>
2019-03-13 18:20:09 +0300

    .

--- Assets/Scripts/UI/MarketManager.cs
+++ Assets/Scripts/UI/MarketManager.cs
@@ -9,7 +9,7 @@ public class MarketManager : MonoBehaviour
     int money, position;
     Image[] buyButtonWeapon, buyButtonBody;
     public Text moneyText;
-    public Image itemPrefab;
+    public Image gunPrefab, bodyPrefab;
     public Image gunPanel, bodyPanel;
     public GameObject popUpGun, popUpBody;
     private Color useColor, buyColor;
@@ -43,13 +43,13 @@ public class MarketManager : MonoBehaviour
     {
         if (v)
         {
-            if (gameObject.transform.position.x < Screen.width)
+            if (Camera.main.ScreenToWorldPoint(gameObject.transform.position).x < 0)
             {
-                popUpBody.transform.position = new Vector3(gameObject.transform.position.x + 50, gameObject.transform.position.y, gameObject.transform.position.z);
+                popUpBody.transform.position = new Vector3(gameObject.transform.position.x + 2, gameObject.transform.position.y, gameObject.transform.position.z);
             }
             else
             {
-                popUpBody.transform.position = new Vector3(gameObject.transform.position.x - 50, gameObject.transform.position.y, gameObject.transform.position.z);
+                popUpBody.transform.position = new Vector3(gameObject.transform.position.x - 2, gameObject.transform.position.y, gameObject.transform.position.z);
             }
             popUpBody.SetActive(true);
         }
@@ -63,7 +63,7 @@ public class MarketManager : MonoBehaviour
     {
         useColor = new Color(0.5f, 1f, 0.5f);
         buyColor = new Color(1, 1, 1);
-        position = 280;
+        //position = 280;
         buyButtonWeapon = new Image[SaveReader.gunCount];
         buyButtonBody = new Image[SaveReader.bodyCount];
         OnLoad();
@@ -78,10 +78,11 @@ public class MarketManager : MonoBehaviour
         for (int i = 0; i < buyButtonWeapon.Length; i++)
         {
             GunState gun = SaveReader.guns[i];
-            Image gunitem = Instantiate(itemPrefab, gunPanel.transform.GetChild(0).transform.GetChild(0).transform);
+            Image gunitem = Instantiate(gunPrefab, gunPanel.transform.GetChild(0).transform.GetChild(0).transform);
             buyButtonWeapon[i] = gunitem;
-            gunitem.rectTransform.anchoredPosition = new Vector3(position, 0, 0);
-            gunitem.GetComponent<MarketItemClickAction>().isGun = true;
+            FillSpecsGun(gunitem, gun, i);
+            //gunitem.rectTransform.anchoredPosition = new Vector3(position, 0, 0);
+            /*gunitem.GetComponent<MarketItemClickAction>().isGun = true;
             gunitem.GetComponent<MarketItemClickAction>().nameGun = gun.name;
             gunitem.GetComponent<MarketItemClickAction>().damage = gun.damage;
             gunitem.GetComponent<MarketItemClickAction>().fireRate = gun.fireRate;
@@ -91,32 +92,65 @@ public class MarketManager : MonoBehaviour
             Image sprite = gunitem.transform.GetChild(1).GetComponent<Image>();      
             cost.text = gun.cost.ToString();
             name.text = gun.name;
-            sprite.sprite = SaveReader.gunSprites[i];
-            position+= 460;
+            sprite.sprite = SaveReader.gunSprites[i];*/
+            //position+= 460;
         }
-        position = 280;
+        //position = 280;
         for (int i = 0; i < buyButtonBody.Length; i++)
         {
             BodyState body = SaveReader.bodies[i];
-            Image gunitem = Instantiate(itemPrefab, bodyPanel.transform.GetChild(0).transform.GetChild(0).transform);
-            buyButtonBody[i] = gunitem;
-            gunitem.rectTransform.anchoredPosition = new Vector3(position, 0, 0);
-            gunitem.GetComponent<MarketItemClickAction>().isGun = false;
-            gunitem.GetComponent<MarketItemClickAction>().nameBody = body.name;
-            gunitem.GetComponent<MarketItemClickAction>().health = body.health;
-            gunitem.GetComponent<MarketItemClickAction>().speed = (int)body.movementSpeed;
-            Text name = gunitem.transform.GetChild(0).GetComponent<Text>();
-            Text cost = gunitem.transform.GetChild(3).GetComponent<Text>();
-            Image sprite = gunitem.transform.GetChild(1).GetComponent<Image>();
+            Image bodyitem = Instantiate(bodyPrefab, bodyPanel.transform.GetChild(0).transform.GetChild(0).transform);
+            buyButtonBody[i] = bodyitem;
+            FillSpecsBody(bodyitem, body, i);
+            //gunitem.rectTransform.anchoredPosition = new Vector3(position, 0, 0);
+            /*bodyitem.GetComponent<MarketItemClickAction>().isGun = false;
+            bodyitem.GetComponent<MarketItemClickAction>().nameBody = body.name;
+            bodyitem.GetComponent<MarketItemClickAction>().health = body.health;
+            bodyitem.GetComponent<MarketItemClickAction>().speed = (int)body.movementSpeed;
+            Text name = bodyitem.transform.GetChild(0).GetComponent<Text>();
+            Text cost = bodyitem.transform.GetChild(3).GetComponent<Text>();
+            Image sprite = bodyitem.transform.GetChild(1).GetComponent<Image>();
             cost.text = body.cost.ToString();
             name.text = body.name;
-            sprite.sprite = SaveReader.bodySprites[i];
-            position += 460;
+            sprite.sprite = SaveReader.bodySprites[i];*/
+            //position += 460;
         }
         //gunPanel.color = new Color(0f, 0.5490f, 1f, 0.65f);
         //bodyPanel.color = new Color(0f, 0.5490f, 1f, 0.25f);
         ChangeActivityBody(false);
         ChangeActivityGun(true);
+    }
+
+    void FillSpecsGun(Image item, GunState gun, int index)
+    {
+        Text name = item.transform.GetChild(0).GetComponent<Text>();
+        Text cost = item.transform.GetChild(3).GetComponent<Text>();
+        Text damage = item.transform.GetChild(5).GetComponent<Text>();
+        Text firerate = item.transform.GetChild(6).GetComponent<Text>();
+        Text bulletspeed = item.transform.GetChild(7).GetComponent<Text>();
+        Image sprite = item.transform.GetChild(1).GetComponent<Image>();
+        name.text = gun.name;
+        cost.text = gun.cost.ToString();
+        damage.text = "Damage : " + gun.damage.ToString();
+        firerate.text = "Fire Rate : " + gun.fireRate.ToString();
+        bulletspeed.text="Bullet Speed : " + gun.bulletSpeed.ToString();
+        sprite.sprite = SaveReader.gunSprites[index];
+    }
+
+
+
+    void FillSpecsBody(Image item, BodyState body, int index)
+    {
+        Text name = item.transform.GetChild(0).GetComponent<Text>();
+        Text cost = item.transform.GetChild(3).GetComponent<Text>();
+        Text health = item.transform.GetChild(5).GetComponent<Text>();
+        Text speed = item.transform.GetChild(6).GetComponent<Text>();
+        Image sprite = item.transform.GetChild(1).GetComponent<Image>();
+        name.text = body.name;
+        cost.text = body.cost.ToString();
+        health.text = "Health : " + body.health.ToString();
+        speed.text = "Speed : " + body.movementSpeed.ToString();
+        sprite.sprite = SaveReader.bodySprites[index];
     }
 
     public void ChangeActivityBody(bool value)
